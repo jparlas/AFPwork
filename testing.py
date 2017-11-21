16493b30c1d9fedc204038a9051a9f824d9188c5
jparlas <jparlas@mlmc.vic.edu.au>
2017-11-21 12:37:39 +1100

    teaching string formating

--- testing.py
+++ testing.py
@@ -1,4 +1,15 @@
 print('Hello world')
 
 name = input('You? ')
+age = int(input('Age ? '))
+
+#the comma simply outputs the result
+print('Hello ', name, age)
+
+#the + tries to construct a string to display
+#and it will only work with int if they are
+#converted to string first
+print('Hello ' + name + str(age))
+
+print('Hello {0} you are {1} years old'.format(name, age))
 
