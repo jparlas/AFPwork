aada5a4caa91b5be7ade22b1c4d808ba1b0b9f34
jparlas <jparlas@mlmc.vic.edu.au>
2017-11-27 13:55:24 +1100

    Created the solution to the file handling exercise

---
+++ listsLoops.py
@@ -0,0 +1,9 @@
+words = []
+sentence = input('Enter a sentence')
+words = sentence.split(' ')
+print(words)
+
+points = []
+gamePoints = input('Enter the points for each game played eg 5,6,7,8')
+points = gamePoints.split(',')
+print(points)
